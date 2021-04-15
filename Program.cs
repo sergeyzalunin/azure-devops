f194181783bd3daf4fb937f5ef8c662c97bd5861
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2021-04-15 22:08:59 +0300

    Push

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World AGAIN!");
+            Console.WriteLine("Hello World AGAIN AND AGAIN!");
         }
     }
 }
